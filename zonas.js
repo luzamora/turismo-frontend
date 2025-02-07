70c834b2c3991d6581c4e4fc7520b91de95c2135
luzamora <luciagargon7@gmail.com>
2025-02-07 19:43:15 +0100

    conexión hecha 2.0

---
+++ zonas.js
@@ -0,0 +1,40 @@
+//cargar las zonas al cargar la página
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const zonas = await getZonas();
+  if (zonas) {
+      printZonas(zonas);
+  }
+});
+
+//funcion que llama al endpoint
+
+const getZonas = async () => {
+  try {
+      const response = await fetch('http://localhost:3000/zonas');
+      if (response.ok) {
+          return await response.json();
+      }
+  } catch (error) {
+      console.error('Ocurrió un error:', error.message);
+      throw new Error('Error al cargar las zonas');
+  }
+  throw new Error('Error en la solicitud');
+};
+
+//HTML
+
+const printZonas = (zonas) => {
+  const list = document.getElementById('zona'); //se conecta al html
+
+// Limpiar las opciones previas, excepto la primera (Elige la zona...)
+list.innerHTML = '<option selected disabled>Elige la zona de Alicante</option>';
+
+// Recorrer el array de zonas y agregarlas como opciones al select
+zonas.forEach(zona => {
+  let option = document.createElement('option'); // Crear opción
+  option.value = zona.id; // Puedes usar un ID si lo tienes en la BD
+  option.textContent = zona.nombre; // Nombre visible de la zona
+  list.appendChild(option); // Agregar al select
+});
+};
